ca5ccd19d9d6b26f0b57983d78daa23d2f10e307
ManiJJ <manashtanmani@gmail.com>
2017-03-19 12:45:04 +0530

    Add files via upload
    'MY FIRST PROGRAMING'

---
+++ Test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test {
+	public static void main(String args[]){
+		for(int i=1;i<5;i++)
+		{
+			for(int j=0;j<i;j++){
+				if(j==3){
+					
+				}
+			}
+				System.out.print("*");
+		}
+		System.out.println(" ");
+		
+	}
+
+}
